c44b1c1e93087f2a1433c86cc6e8a23323e95505
Madhusanka Edirimanna <48412959+Edirimanna@users.noreply.github.com>
2022-01-05 15:07:13 +0530

    Update linked list file

--- JavaCollections/src/com/company/LinkedListExample.java
+++ JavaCollections/src/com/company/LinkedListExample.java
@@ -5,9 +5,20 @@ import java.util.LinkedList;
 public class LinkedListExample {
     public static void main(String args[]){
         LinkedList<String> animal=new LinkedList<String>();
+
+        //adding element to the Linked list
         animal.add("Dog");
         animal.add("Cat");
         animal.add("Crow");
+        animal.addFirst("lion");
+        animal.addLast("Rabbit");
         System.out.println(animal);
+
+        //Remove index 2 element
+        animal.remove(2);
+        //Display after remove element
+        for (String i:animal) {
+            System.out.println(i);
+        }
     }
 }
